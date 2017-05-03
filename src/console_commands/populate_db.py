55b080c6e38b8eda18d08c96ea9eb3de8162fd21
Cormac Phelan <cormac@riffstation.com>
2017-05-03 17:56:58 +0100

    Updates populate_db command

--- src/console_commands/populate_db.py
+++ src/console_commands/populate_db.py
@@ -5,18 +5,11 @@ from pgsqlutils.orm import Session
 def run(**kwargs):
     factory = Factory(Session)
 
+    print("Generating sample data:")
+
     # artist data
+    print("artists...")
     factory.clear_artists()
     factory.get_artist()
 
-    # # chord data
-    # factory.clear_chords()
-    # factory.create_chords()
-    #
-    # # song data
-    # factory.clear_songs()
-    # factory.create_songs()
-    #
-    # # songfeed data
-    # factory.clear_songfeeds()
-    # factory.create_songfeeds()
+    print("Sample db population complete...")
