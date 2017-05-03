2b50c2849b1a549972a285d9acf75e9eb7cb414e
Cormac Phelan <cormac@riffstation.com>
2017-05-03 15:34:00 +0100

    Adds artist url routes

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,7 +1,8 @@
-from .views.api import ApiDescription, GenreResourceView
+from .views.api import ApiDescription, GenreResourceView, ArtistResourceView
 
 URLS = (
     ('/', ApiDescription, 'api_description'),
     ('/genres', GenreResourceView, 'genres'),
-
+    ('/artists', ArtistResourceView, 'artists'),
+    ('/artists/<uuid:uuid>', ArtistResourceView, 'specific_artist'),
 )
