57e65613d68c22847f609ca23b1ebaabf0d4c7c8
Cormac Phelan <cormac@riffstation.com>
2017-05-03 15:33:24 +0100

    Adds Artist model

--- src/app/models.py
+++ src/app/models.py
@@ -1,8 +1,23 @@
 from flaskutils.models import FlaskModel
-from sqlalchemy import Column, String
+from sqlalchemy import (Column, String, Boolean)
+from sqlalchemy.dialects.postgresql import (
+    ARRAY, JSON, UUID
+)
 
 
 class Genre(FlaskModel):
     __tablename__ = 'genre'
     name = Column(String(256))
     description = Column(String(256))
+
+
+class Artist(FlaskModel):
+    __tablename__ = 'artists'
+    key = Column(UUID(as_uuid=True), nullable=False, unique=True)
+    name = Column(String(256), nullable=False, unique=True)
+    image = Column(String(256))
+    members = Column(ARRAY(UUID))
+    related_artists = Column(ARRAY(UUID))
+    is_popular = Column(Boolean, default=False)
+    first_character = Column(String(16))
+    extra_params = Column(JSON)
