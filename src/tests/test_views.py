e4d5287e631bb40704a1d1499bb028b24ca17ae5
Cormac Phelan <cormac@riffstation.com>
2017-05-03 17:57:37 +0100

    Adds artist PUT unit tests

--- src/tests/test_views.py
+++ src/tests/test_views.py
@@ -50,10 +50,10 @@ class TestAppCase(TransactionalTestCase):
 
         # Traverse into the 'artist' block of JSON
         data = data['artist']
-        # Test required values: key, name
-        assert 'key' in data
-        assert data['key'] is not None
-        assert data['key'] == str(artist.key)
+        # Test required values: id, name
+        assert 'id' in data
+        assert data['id'] is not None
+        assert data['id'] == str(artist.key)
         assert 'name' in data
         assert data['name'] is not None
         assert data['name'] == artist.name
@@ -70,6 +70,91 @@ class TestAppCase(TransactionalTestCase):
             headers=self.json_request_headers)
 
         assert 404 == result.status_code
+
+    def test_put_artist_valid(self):
+        """
+        Makes a PUT method request and validates responses
+        """
+        existing_artist = self.factory.get_artist()
+        init_artist_count = Artist.objects.count()
+
+        # construct a valid JSON body for a PUT artist request
+        put_request_body = {
+            "id": str(existing_artist.key),
+            "name": "NewArtistName",
+            "first_character": "N",
+            "extra_params": {"fieldName": "fieldValue"},
+            "image": "imageURL",
+            "members": [
+                "11c03524-1911-433f-bf86-f234cacd5bcf",
+                "a503faf9-45b5-4fec-8334-337284a66ea4"
+            ],
+            "related_artists": [
+                "079117d5-8fcc-4f11-82d8-0f975a408b12",
+                "3d49a1f9-3b1f-491c-b504-a5f4190b802c"
+            ],
+            "is_popular": True
+        }
+        request_url = '/artists/' + str(existing_artist.key)
+
+        result = self.client.put(
+            request_url, data=json.dumps(put_request_body),
+            headers=self.json_request_headers)
+
+        # Test for valid response code
+        assert 200 == result.status_code
+        response_data = json.loads(result.get_data().decode('utf-8'))
+        # Test for non-empty response JSON
+        assert 'artist' in response_data
+        # Traverse into the 'artist' block of JSON
+        artist_data = response_data['artist']
+        # Test required values: id, name
+        assert 'id' in artist_data
+        assert 'name' in artist_data
+        assert Artist.objects.count() == init_artist_count
+
+        # Verify that updated record reflects new values
+        get_url = '/artists/' + str(existing_artist.key)
+        result = self.client.get(get_url, headers=self.json_request_headers)
+        get_response_data = json.loads(result.get_data().decode('utf-8'))
+        assert 'artist' in get_response_data
+        updated_obj = Artist.objects.get(
+            key=get_response_data['artist']['id'])
+        assert updated_obj.id == existing_artist.id
+        assert updated_obj.name == 'NewArtistName'
+
+    def test_put_artist_unknown_uuid(self):
+        """
+        Makes a PUT method request and validates responses
+        """
+        self.factory.get_artist()
+        init_artist_count = Artist.objects.count()
+        new_uuid = uuid.uuid4()
+
+        # construct a valid JSON body for a PUT artist request
+        put_request_body = {
+            "id": str(new_uuid),
+            "name": "NewArtistName",
+            "image": "imageURL",
+            "members": [
+                "11c03524-1911-433f-bf86-f234cacd5bcf",
+                "a503faf9-45b5-4fec-8334-337284a66ea4"
+            ],
+            "related_artists": [
+                "079117d5-8fcc-4f11-82d8-0f975a408b12",
+                "3d49a1f9-3b1f-491c-b504-a5f4190b802c"
+            ],
+            "is_popular": True
+        }
+        request_url = '/artists/' + str(new_uuid)
+
+        result = self.client.put(
+            request_url, data=json.dumps(put_request_body),
+            headers=self.json_request_headers)
+
+        # Test for valid response code
+        assert 404 == result.status_code
+        assert Artist.objects.count() == init_artist_count
 
     def test_post_genre_serializer(self):
         """
